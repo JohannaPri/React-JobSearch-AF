2988b4fd37b8ecfc861fb9a3d4c6456b5b76e16e
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-09-24 13:42:15 +0200

    :sparkles: add interface jobAd

---
+++ src/models/IJobAd.ts
@@ -0,0 +1,31 @@
+import { IJobAdDescription } from "./IJobAdDescription";
+
+export interface IJobAd {
+    id: string;
+    headline: string;
+    application_deadline: string;
+    number_of_vacancies: string;
+    description: IJobAdDescription;
+    employment_type: IJobTechTaxonomyItem;
+    salary_type: IJobTechTaxonomyItem;
+    duration: IJobTechTaxonomyItem;
+    working_hours_type: IJobTechTaxonomyItem;
+    scope_of_work: IScopeOfWork;
+    employer: IEmployer;
+    application_details: IApplicationDetails;
+    experience_required: boolean;
+    access_to_own_car: boolean;
+    driving_license_required: boolean;
+    occupation: IJobTechTaxonomyItem;
+    occupation_group: IJobTechTaxonomyItem;
+    occupation_field: IJobTechTaxonomyItem;
+    occupation_address: IJobTechTaxonomyItem;
+    workplace_address: IWorkPlaceAddress;
+    must_have: IRequirements;
+    nice_to_have: IRequirements;
+    publication_date: string;
+    last_publication_date: string;
+    removed: boolean;
+    removed_date: string;
+    timestamp: BigInteger;
+}
