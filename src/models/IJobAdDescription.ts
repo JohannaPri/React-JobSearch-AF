355f37847d6014fd74bf70d145d4d011bcbb7289
Augustine Al-Zebary <146852586+Ogen1998@users.noreply.github.com>
2024-09-24 13:05:27 +0200

    Update IJobAdDescription.ts

--- src/models/IJobAdDescription.ts
+++ src/models/IJobAdDescription.ts
@@ -1,9 +1,9 @@
-interface IJobAdDescription {
-    text: string;
-    text_formatted: string;
-    company_information: string;
-    needs: string;
-    requirements: string;
-    conditions: string;
-  }
+// interface IJobAdDescription {
+//     text: string;
+//     text_formatted: string;
+//     company_information: string;
+//     needs: string;
+//     requirements: string;
+//     conditions: string;
+//   }
   
