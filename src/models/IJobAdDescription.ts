400579eba6801161425b0b2e4fa6200e44d28c56
Augustine Al-Zebary <146852586+Ogen1998@users.noreply.github.com>
2024-09-24 13:01:46 +0200

    Created IJobAdDescription model

---
+++ src/models/IJobAdDescription.ts
@@ -0,0 +1,9 @@
+interface IJobAdDescription {
+    text: string;
+    text_formatted: string;
+    company_information: string;
+    needs: string;
+    requirements: string;
+    conditions: string;
+  }
+  
