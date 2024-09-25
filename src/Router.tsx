410799d85c352e477ff3c4216628e035a2e77a56
Johanna Prinz <Johannaa.L@hotmail.com>
2024-09-25 14:15:21 +0200

    ✨ Added export function and first content

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,31 +1,38 @@
 import { createBrowserRouter } from "react-router-dom";
+import { NotFound } from "./pages/NotFound";
+import { Layout } from "./pages/Layout";
+import { Start } from "./pages/Start";
+import { JobsApp } from "./pages/JobsApp";
+import { Job } from "./pages/Job";
+import { StatisticsApp } from "./pages/StatisticsApp";
+import { Statistic } from "./pages/Statistic";
 
-export const router = createBrowserRouter ({
-    {
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    errorElement: <NotFound></NotFound>,
+    children: [
+      {
         path: "/",
-        element: <Layout></Layout>,
-        errorElement: <NotFound></NotFound>,
-        children: [
-            {
-                path: "/",
-                element: <Start></Start>,
-            },
-            {
-                path: "/searchjobs"
-                element: <JobsApp></JobsApp>,
-            },
-            {
-               path: "/searchjob/:id",
-               element: <Job></Job>,
-            },
-            {
-                path: "/searchstatistics",
-                element: <StatistiscApp></StatisticsApp>,
-            },
-            {
-                path: "/searchstatistic/:id",
-                element: <Statistic></Statistic>
-            },
-        ]
-    }
-});
+        element: <Start></Start>,
+      },
+      {
+        path: "/searchjobs",
+        element: <JobsApp></JobsApp>,
+      },
+      {
+        path: "/searchjob/:id",
+        element: <Job></Job>,
+      },
+      {
+        path: "/searchstatistics",
+        element: <StatisticsApp></StatisticsApp>,
+      },
+      {
+        path: "/searchstatistic/:id",
+        element: <Statistic></Statistic>,
+      },
+    ],
+  },
+]);
---
+++ src/pages/StatisticsApp.tsx
@@ -0,0 +1,3 @@
+export const StatisticsApp = () => {
+  return <>StatisticsApp</>;
+};
