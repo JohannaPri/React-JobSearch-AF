14cef1f28e7fb93272af0cb9764800802e0f7b2e
Augustine Al-Zebary <146852586+Ogen1998@users.noreply.github.com>
2024-09-27 11:28:15 +0200

    styled and animated NotFound page/component

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,40 @@
+import { ErrorPageStatusCodes } from "@digi/arbetsformedlingen";
+import {
+  DigiNotificationErrorPage,
+  DigiLinkInternal,
+} from "@digi/arbetsformedlingen-react";
+import { NavLink } from "react-router-dom";
+
 export const NotFound = () => {
-  return <>NotFound</>;
+  return (
+    <div className="notFound">
+      <div>
+        <DigiNotificationErrorPage
+          afHttpStatusCode={ErrorPageStatusCodes.NOT_FOUND}
+        >
+          <p slot="bodytext">
+            Det kan bero på att länken hit är felaktig eller att sidan inte
+            finns kvar. Prova att ladda om sidan, felet kan vara tillfälligt. Du
+            kan använd länken nedan för att gå tillbaka till startsidan.
+          </p>
+
+          <ul slot="links">
+            <li>
+              <DigiLinkInternal afHref="/" afVariation="small">
+                <NavLink to={"/"}>Gå till startsidan</NavLink>
+              </DigiLinkInternal>
+            </li>
+          </ul>
+        </DigiNotificationErrorPage>
+      </div>
+      <div>
+        <img
+          className="brokenCompass"
+          height={300}
+          src="../public/not-found-compass.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
