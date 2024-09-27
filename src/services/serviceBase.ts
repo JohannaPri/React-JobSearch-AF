8022aafde81404913067f4dec5e5669a6722119f
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-09-27 15:19:04 +0200

    :sparkles:  finish up input filter

--- src/services/serviceBase.ts
+++ src/services/serviceBase.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-export const get = async <T>(url:string) => {
-    return await axios.get<T>(url);
+
+export const get = async <T>(url:string):  Promise<T> => {
+    const response = await axios.get<T>(url);
+    return response.data
 };
