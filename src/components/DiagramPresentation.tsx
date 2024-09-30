5d2c0b3a7afe8d758e6b9360b7a92f7bb0ecf3e5
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-09-30 15:03:32 +0200

    :sparkles: add diagram data

--- src/components/DiagramPresentation.tsx
+++ src/components/DiagramPresentation.tsx
@@ -1,7 +1,37 @@
-export const DiagramPresentation = () => {
-    
+import {
+  DigiBarChart,
+  DigiLayoutContainer,
+} from "@digi/arbetsformedlingen-react";
+import { ChartLineData } from "@digi/arbetsformedlingen/dist/types/interfaces";
 
-    return <>
-           Diagram med historik
+export const DiagramPresentation = () => {
+  // använd contextet
+  const mockData = {
+    total: {
+      value: 492750,
+    },
+    positions: 1004895,
+    query_time_in_millis: 67,
+    result_time_in_millis: 81,
+    stats: [],
+    freetext_concepts: {},
+    hits: [],
+  };
+
+  const chartData: ChartLineData = {
+    data: {
+      xValues: [2020, 2021],
+      series: [{ yValues: [mockData.total.value, 20], title: "Antal Jobb" }],
+    },
+    x: "År",
+    y: "Antal Jobb",
+    title: "Diagram",
+  };
+  return (
+    <>
+      <DigiLayoutContainer>
+        <DigiBarChart afChartData={chartData}></DigiBarChart>
+      </DigiLayoutContainer>
     </>
-}
+  );
+};
--- src/pages/StatisticsApp.tsx
+++ src/pages/StatisticsApp.tsx
@@ -1,17 +1,18 @@
-//import { DiagramPresentation } from "../components/DiagramPresentation";
 import { HistoricalJobs } from "../components/HistoricalJobs";
+
+import { DiagramPresentation } from "../components/DiagramPresentation";
 //import { InputSearchStatistics } from "../components/InpuSearchStatistics";
 
 export const StatisticsApp = () => {
-
   // lägg contextet här runt Presentations taggarna. och använd reducern som value
-
   // hämta data med IJobHistoricalAd niterface com model
-  
   /*<InputSearchStatistics />
-      <DiagramPresentation />*/
-  return <>
-      
+   */
+
+  return (
+    <>
+      <DiagramPresentation />
       <HistoricalJobs />
-  </>;
+    </>
+  );
 };
