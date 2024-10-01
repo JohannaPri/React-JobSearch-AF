ece93e1cf500830d7e20814fe259304c82e8712d
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-10-01 10:35:21 +0200

    :sparkles: add total jobs och positions

--- src/components/Jobs.tsx
+++ src/components/Jobs.tsx
@@ -1,12 +1,24 @@
 import { useContext } from "react";
 import { JobsContext } from "../contexts/JobsContext";
 import { JobsPresentation } from "./JobsPresentation";
+import {
+  DigiLayoutContainer,
+  DigiTypography,
+} from "@digi/arbetsformedlingen-react";
 
 export const Jobs = () => {
   const { jobs } = useContext(JobsContext);
 
   return (
     <>
+      <DigiLayoutContainer>
+        <DigiTypography>
+          <div className="total-presentation">
+            <p>Totalt antal anonser : {jobs.total.value}</p>
+            <p>Totalt antal jobb: {jobs.positions}</p>
+          </div>
+        </DigiTypography>
+      </DigiLayoutContainer>
       <div>
         {jobs.hits.map((job) => (
           <div key={job.id}>
