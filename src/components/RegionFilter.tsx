69aa4c66fdb510592ef6b5cecfaca5f82f2518e0
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-10-01 14:48:05 +0200

    :sparkles: add aria-label

--- src/components/RegionFilter.tsx
+++ src/components/RegionFilter.tsx
@@ -1,38 +1,42 @@
-import { DigiFormFilter } from "@digi/arbetsformedlingen-react"
+import { DigiFormFilter } from "@digi/arbetsformedlingen-react";
 import { useState, useEffect } from "react";
 import { TaxonomyRegions } from "../models/ITaxonomyRegion";
 import { getTaxonomyRegions } from "../services/jobSearchService";
 import { IUserFilter } from "../models/IUserFilter";
 
 export interface IFilterProps {
- userFilter: IUserFilter
+  userFilter: IUserFilter;
 }
 
-export const RegionFilter = ({userFilter}: IFilterProps) => {
+export const RegionFilter = ({ userFilter }: IFilterProps) => {
+  const [taxonomy, setTaxonomy] = useState<TaxonomyRegions[] | undefined>([]);
+  const [fetched, setFetched] = useState(false);
 
-    const [taxonomy, setTaxonomy] = useState<TaxonomyRegions[] | undefined>([]);
-    const [fetched, setFetched] =useState(false);
-  
-    useEffect(() => {
-      if(fetched) return
-      const getRegions = async () => {
-        const data = await getTaxonomyRegions();
-        setTaxonomy(data)
-        setFetched(true)
-      }
-  
-      getRegions()
-    })
-    
+  useEffect(() => {
+    if (fetched) return;
+    const getRegions = async () => {
+      const data = await getTaxonomyRegions();
+      setTaxonomy(data);
+      setFetched(true);
+    };
 
-    return <>
-    <DigiFormFilter
-          afFilterButtonText="Välj Län"
-          afSubmitButtonText="Filtrera"
-          afListItems={taxonomy?.map((tax) => ( {id: tax['taxonomy/id'], label: tax["taxonomy/preferred-label"]}))}
-          onAfSubmitFilter={(e) => userFilter.region = e.detail.checked}
-          onAfCloseFilter={(e) => userFilter.region = e.detail.checked}
-        ></DigiFormFilter>
-    
+    getRegions();
+  });
+
+  return (
+    <>
+      <DigiFormFilter
+        afFilterButtonText="Välj Län"
+        afSubmitButtonText="Filtrera"
+        afFilterButtonAriaLabel="Välj län att filtrera sökningen med"
+        afSubmitButtonTextAriaLabel="Filtrera sökningen"
+        afListItems={taxonomy?.map((tax) => ({
+          id: tax["taxonomy/id"],
+          label: tax["taxonomy/preferred-label"],
+        }))}
+        onAfSubmitFilter={(e) => (userFilter.region = e.detail.checked)}
+        onAfCloseFilter={(e) => (userFilter.region = e.detail.checked)}
+      ></DigiFormFilter>
     </>
-}
+  );
+};
