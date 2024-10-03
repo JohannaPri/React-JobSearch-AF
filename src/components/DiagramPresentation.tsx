c519d9b5ca54408818cf4eb0829ac02e703c8fac
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-10-03 08:45:26 +0200

    :sparkles: diagram funktionallity

--- src/components/DiagramPresentation.tsx
+++ src/components/DiagramPresentation.tsx
@@ -12,24 +12,31 @@ import { IJobAd } from "../models/IJobAd";
 export const DiagramPresentation = () => {
   const { jobs } = useContext(JobsHistoryContext);
 
-  const sorted = jobs.hits.sort((a, b) => {
-    const dateA = a.publication_date
-      ? new Date(a.publication_date).getTime()
-      : 0;
-    const dateB = b.publication_date
-      ? new Date(b.publication_date).getTime()
-      : 0;
-    return dateB - dateA;
-  });
-  console.log(sorted);
+  const groupedByYear = jobs.hits.reduce(
+    (acc: { [key: string]: IJobAd[] }, current: IJobAd) => {
+      const year = current.publication_date?.split("-")[0]
+        ? new Date(current.publication_date).getFullYear()
+        : 0;
+      if (!acc[year]) {
+        acc[year] = [];
+      }
+      acc[year].push(current);
+      return acc;
+    },
+    {}
+  );
+
+  const stringYears = Object.keys(groupedByYear);
+  const numberYears = Object.keys(groupedByYear).map((year) => +year);
+  const values = numberYears.map((year) => groupedByYear[year].length);
+
+  console.log(values);
 
   const chartData: ChartLineData = {
     data: {
-      xValues: [2019, 2020, 2021],
-      series: [
-        { yValues: [50, 10, 20], title: "Antal Jobb", colorToken: "black" },
-      ],
-      xValueNames: ["2019", "2020", "2021"],
+      xValues: numberYears,
+      series: [{ yValues: values, title: "Antal Jobb", colorToken: "black" }],
+      xValueNames: stringYears,
     },
     x: "År",
     y: "Antal Jobb",
