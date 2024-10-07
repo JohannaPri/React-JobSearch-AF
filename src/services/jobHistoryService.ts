6ac7a6ebb376e522178246e544d6b864932a5e70
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-10-07 13:25:36 +0200

    :sparkles: fixing digram bug

--- src/services/jobHistoryService.ts
+++ src/services/jobHistoryService.ts
@@ -1,64 +1,4 @@
-// import { get } from "./serviceBase";
-// import { IHistoricalSearchFilter } from "../models/IHistoricalSearchFilter";
-// import { IHistoricalSearchResult } from "../models/IHistoricalSearchResults";
 
-// const BASE_URL = "https://historical.api.jobtechdev.se/search?";
-
-// export const getHistoricalJobs = async (
-//   userInput: IHistoricalSearchFilter
-// ): Promise<IHistoricalSearchResult[]> => {
- 
-//   const years = [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023];
-//   const yearFrom = userInput.dateFrom.getFullYear();
-//   const yearTo = userInput.dateTo.getFullYear();
-
-
-
-//   const filteredYears = years.filter(
-//     (year) => year >= yearFrom && year <= yearTo
-//   );
-
-//   const promises = filteredYears.map(async (year) => {
-//     const queryParams: string[] = [];
-
-//     // Add search text to query params if present
-//     if (userInput.searchText) {
-//       queryParams.push(`q=${encodeURIComponent(userInput.searchText)}`);
-//     }
-//     // Add dateFrom to query params
-//     if (userInput.dateFrom) {
-//       queryParams.push(
-//         `historical-from=${encodeURIComponent(year)}T00%3A00%3A01`
-//       );
-//     }
-//     // Add dateTo to query params, without incrementing year incorrectly
-//     if (userInput.dateTo) {
-//       queryParams.push(
-//         `historical-to=${encodeURIComponent(year)}T23%3A59%3A59`
-//       );
-//     }
-
-//     // Add limit
-//     queryParams.push("limit=10");
-
-//     // Construct the final URL
-//     const finalUrl = `${BASE_URL}${queryParams.join("&")}`;
-  
-//     // Return the promise from getData, which resolves with the data
-//     return getData(finalUrl);
-//   });
-//   const finalData = await Promise.all(promises);
-//   console.log(finalData); 
-
-// const getData = async (finalUrl:string): Promise<IHistoricalSearchResult[]> => {
-//   try {
-//     const response = await get<IHistoricalSearchResult[]>(`${finalUrl}`);
-//     return response // Spread the array if it's an array
-    
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
 import { get } from "./serviceBase";
 import { IHistoricalSearchFilter } from "../models/IHistoricalSearchFilter";
 import { IHistoricalSearchResult } from "../models/IHistoricalSearchResults";
@@ -94,14 +34,13 @@ export const getHistoricalJobs = async (
     if (userInput.dateTo) {
       queryParams.push(
         `historical-to=${encodeURIComponent(year + 1)}T00%3A00%3A01`
-      ); // year + 1 instead of modifying the year variable directly
+      ); 
     }
 
     queryParams.push("limit=10");
 
     const finalUrl = `${BASE_URL}${queryParams.join("&")}`;
-  
-    // Await the data fetching for each year
+
     await getData(year, finalUrl, finalData);
   }
 
